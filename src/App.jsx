11bb7abcba65fe61ffc6607987cff0cdb2602755
samuelphua <samuel.phua@hotmail.com>
2023-02-02 14:47:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Shop from './Components/Shop';
+import React from "react";
+import Shop from "./Components/Shop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Shop />
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="Shop" element={<h1>Shop</h1>}></Route>
+          <Route path="About us" element={<h1>About us</h1>}></Route>
+          <Route path="Bulk Orders" element={<h1>Bulk Orders</h1>}></Route>
+          <Route path="Contact Us" element={<h1>Contact us</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
-};
+}
 
 export default App;
