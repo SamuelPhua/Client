a8aff304d9336e63a51bba6135ab1e533cbe540e
mervin-njy <mervin.njy@outlook.com>
2023-02-06 10:26:49 +0800

    destructured props in banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Banner = (props) => {
+const Banner = ({ img, title }) => {
   // TODO : set up props to get background image and header from parent/ url
   return (
     <div
-      className={`flex justify-center items-center bg-[url(${props.img})] h-[300px] mb-6 bg-cover bg-no-repeat`}
+      className={`flex justify-center items-center bg-[url(${img})] h-[300px] mb-6 bg-cover bg-no-repeat`}
     >
       <h1 className="font-permanentMarker text-darkBlueFont tracking-wider text-5xl">
-        {props.title}
+        {title}
       </h1>
     </div>
   );
