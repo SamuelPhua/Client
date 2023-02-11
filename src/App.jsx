186ff61e5946981b68f94487fb93131f54623256
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-02-11 16:46:18 +0800

    Added navigation to product page from Shop.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Shop from "./Components/Shop";
+
 import { Routes, Route } from "react-router-dom";
-import AboutUs from "./Components/AboutUs";
 import ShippingAlert from "./Components/ShippingAlert";
 import Header from "./Components/Header";
+import Shop from "./Components/Shop";
+import Product from "./Components/Product";
+import AboutUs from "./Components/AboutUs";
+import BulkOrders from "./Components/BulkOrders";
 import ContactUs from "./Components/ContactUs";
 import FAQ from "./Components/footerPages/FAQ";
 import PrivacyPolicy from "./Components/footerPages/PrivacyPolicy";
 import TermsAndConditions from "./Components/footerPages/TermsAndConditions";
 import Footer from "./Components/Footer";
-import BulkOrders from "./Components/BulkOrders";
 import Shipping from "./Components/Shipping";
 
 function App() {
@@ -18,22 +20,9 @@ function App() {
       <ShippingAlert />
       <Header />
       <Routes>
-        <Route
-          path="shop"
-          element={
-            <h1>
-              <Shop />
-            </h1>
-          }
-        ></Route>
-        <Route
-          path="about-us"
-          element={
-            <h1>
-              <AboutUs />
-            </h1>
-          }
-        ></Route>
+        <Route path="shop" element={<Shop />}></Route>
+        <Route path="product/:name" element={<Product />}></Route>
+        <Route path="about-us" element={<AboutUs />}></Route>
         <Route path="bulk-orders" element={<BulkOrders />}></Route>
         <Route path="contact-us" element={<ContactUs />}></Route>
         <Route path="faq" element={<FAQ />}></Route>
