2a22679fa140717c27a4a4f68b187d32d5f05682
mervin-njy <mervin.njy@outlook.com>
2023-02-13 07:58:55 +0800

    changed format to fit

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -13,23 +13,33 @@ const Product = () => {
   return (
     <>
       {/* #1 Shop > Cookie name links */}
-      <div className="w-10/12 px-10 mt-10 mx-auto">
+      <div className="flex flex-wrap w-7/10 mx-auto mt-10">
+        <p
+          className="hover:text-orange cursor-pointer tracking-wide font-montserrat text-darkBlueFont w-20 text-xxs md:text-xxs mb-1"
+          onClick={navigateToShop}
+        >
+          Shop
+        </p>
+        <p className="tracking-wide font-montserrat text-darkBlueFont w-12 text-xxs md:text-xxs mb-1">
+          &#62;
+        </p>
         <p className="tracking-wide font-montserrat text-darkBlueFont text-xxs md:text-xxs mb-1">
-          <span
-            className="hover:text-orange cursor-pointer"
-            onClick={navigateToShop}
-          >
-            Shop
-          </span>{" "}
-          &#62; {name}
+          {name}
         </p>
       </div>
 
       {/* #2 flex div */}
+      <div className="flex flex-wrap w-7/10 mx-auto mt-10">
+        {/* #3 LEFT: Cookie displays */}
+        <div className="w-5/12">
+          <div className="bg-slate-600">HELLO</div>
+        </div>
 
-      {/* #3 LEFT: Cookie displays */}
-
-      {/* #4 Cookie options and descriptions */}
+        {/* #4 Cookie options and descriptions */}
+        <div className="w-7/12">
+          <div className="bg-slate-600">HELLO</div>
+        </div>
+      </div>
     </>
   );
 };
