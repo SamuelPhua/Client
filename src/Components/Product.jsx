b57c34b9433043a8e733b93e8f80302138056bcb
mervin-njy <mervin.njy@outlook.com>
2023-02-13 01:01:57 +0800

    Add navigation links and simple css for Shop > Cookie name

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,37 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
-const Product = (props) => {
+const Product = () => {
+  // params :name to get cookie product
   const { name } = useParams();
-  return <div>Product: {name}</div>;
+  // Navigate back to the shop all page
+  const navigate = useNavigate();
+  const navigateToShop = () => {
+    navigate("/shop");
+  };
+
+  return (
+    <>
+      {/* #1 Shop > Cookie name links */}
+      <div className="w-10/12 px-10 mt-10 mx-auto">
+        <p className="tracking-wide font-montserrat text-darkBlueFont text-xxs md:text-xxs mb-1">
+          <span
+            className="hover:text-orange cursor-pointer"
+            onClick={navigateToShop}
+          >
+            Shop
+          </span>{" "}
+          &#62; {name}
+        </p>
+      </div>
+
+      {/* #2 flex div */}
+
+      {/* #3 LEFT: Cookie displays */}
+
+      {/* #4 Cookie options and descriptions */}
+    </>
+  );
 };
 
 export default Product;
