4ba2e712c5100d82926283274942a4cf13b3af61
mervin-njy <mervin.njy@outlook.com>
2023-02-13 00:37:15 +0800

    imported useParams to indiv product page + change Shipping to Checkout in  Checkout Component

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = (props) => {
+  const { name } = useParams();
+  return <div>Product: {name}</div>;
 };
 
 export default Product;
