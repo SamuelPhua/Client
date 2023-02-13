eeefe2eaa05499aff80247a92a47359cbecdc378
kellyn <kellynwong@hotmail.com>
2023-02-13 23:32:31 +0800

    Cart and CartProduct CSS updates done

--- src/Components/CartProduct.jsx
+++ src/Components/CartProduct.jsx
@@ -8,47 +8,49 @@ const CartProduct = ({ name, price, weight, packaging, quantity, index }) => {
     useContext(DataContext);
 
   return (
-    <div>
+    <>
       <div className="w-11/12 grid grid-cols-7 ml-20 p-10 border-b-2 border-[#999999] ">
         <div className="col-start-1 flex justify-center items-center">
           <img className="w-48 h-48" src={placeholderImg} />
         </div>
-      </div>
-      <div className="ml-20 col-start-2 col-end-4 flex">
-        <div className="self-center text-[1.5rem] font-montserrat text-lightBlue">
-          <span className="font-bold">{name}</span>
-          <p className="mt-5 text-[1.3rem]">{weight}</p>
-          <p className="mt-5 text-[1.3rem]">{packaging}</p>
+
+        <div className="ml-20 col-start-2 col-end-4 flex">
+          <div className="self-center text-[1.2rem] font-montserrat text-lightBlue">
+            <div className="font-bold">{name}</div>
+            <div className="mt-5 text-[1.2rem]">{weight}</div>
+            <div className="mt-5 text-[1.2rem]">{packaging}</div>
+          </div>
+        </div>
+
+        <div className="justify-center items-center col-start-5 flex">
+          <div>
+            <AiFillMinusCircle
+              className="inline-block text-lightBlue text-5xl"
+              onClick={() => handleMinusQty(index)}
+            />
+            <div className="inline-block ml-8 mr-8 text-darkBlueFont text-[1.2rem]">
+              {quantity}
+            </div>
+            <AiFillPlusCircle
+              className="inline-block text-lightBlue text-5xl"
+              onClick={() => handlePlusQty(index)}
+            />
+            <div
+              className="block text-[0.9rem] font-montserrat text-center text-darkBlueFont underline"
+              onClick={() => handleDelete(index)}
+            >
+              Remove
+            </div>
+          </div>
+        </div>
+
+        <div className="col-start-7 flex justify-end items-center">
+          <div className="text-[1.3rem] font-montserrat text-lightBlue tracking-wider">
+            ${quantity * price}
+          </div>
         </div>
       </div>
-
-      <div className="justify-center items-center col-start-5 flex">
-        <div>
-          <AiFillMinusCircle className="inline-block text-lightBlue text-6xl" onClick={() => handleMinusQty(index)} />
-          <p
-            className="inline-block ml-8 mr-8 text-darkBlueFont text-[1.5rem]"
-          >
-            {quantity}
-          </p>
-          <AiFillPlusCircle
-            className="inline-block text-lightBlue text-6xl"
-            onClick={() => handlePlusQty(index)}
-          />
-          <p
-            className="block text-[1rem] font-montserrat text-center text-darkBlueFont underline"
-            onClick={() => handleDelete(index)}
-          >
-            Remove
-          </p>
-        </div>
-      </div>
-
-      <div className="col-start-7 flex justify-end items-center">
-        <h2 className="text-[1.5rem] font-montserrat text-lightBlue">
-          ${quantity * price}
-        </h2>
-      </div>
-    </div>
+    </>
   );
 };
 
