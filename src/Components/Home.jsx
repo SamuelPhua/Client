64a02ccab5cb15ddaaa0e008ca7ee690a3b43274
Leon <sathyarajthevar@gmail.com>
2023-02-13 10:36:29 +0800

    changing some text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -26,22 +26,22 @@ const Home = () => {
 
         <div class="grid grid-cols-3 gap-4 px-4 max-w-6xl mx-auto mt-20">
           <div class="col-span-3 flex justify-center">
-            <h1 class="text-4xl font-bold uppercase">Kittens!</h1>
+            <h1 class="text-4xl font-bold uppercase">Best Sellers!</h1>
           </div>
 
           <div class="col-span-3 md:col-span-1 flex flex-col items-center">
             <img src="src\assets\imagesShop\image1.png" class="rounded-full " />
-            <h2>Kitten 1</h2>
+            <h2>cookie1</h2>
           </div>
 
           <div class="col-span-3 md:col-span-1 flex flex-col items-center">
             <img src="src\assets\imagesShop\image2.png" class="rounded-full" />
-            <h2>Kitten 2</h2>
+            <h2>cookie2</h2>
           </div>
 
           <div class="col-span-3 md:col-span-1 flex flex-col items-center">
             <img src="src\assets\imagesShop\image3.png" class="rounded-full" />
-            <h2>Kitten 3</h2>
+            <h2>cookie3</h2>
           </div>
         </div>
 
