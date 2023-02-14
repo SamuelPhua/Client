b0bd41b168c820bffcfb71972b12b0b84fa090da
samuelphua <samuel.phua@hotmail.com>
2023-02-15 01:31:37 +0800

    clean codes

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,52 +1,13 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import ButtonOrange from "./reusables/ButtonOrange";
 import CartProduct from "./CartProduct";
-import DataContext from "./context/DataContext";
 
 const Cart = ({ shoppingCart }) => {
-  const data = useContext(DataContext);
-  /*
-   ** Functionality of the cart must check if the items added to Cart have any stock from Product Page
-   ** We are able to edit the quantity of the added items
-   ** Clicking the images will route back to the Product Page ?? *Must Check -- this one isn't done yet
-   ** The total amount of all the added items must appear inside
-   ** Local Storage
-   ** A button to route to the checkout page
-   */
-
-  const [cartItems, setCartItems] = useState([]);
-  // const [deleteFromCart, setDeleteFromCart] = useState([]);
-  const [count, setCount] = useState(1);
-
   // Navigate to the checkout page
   const navigate = useNavigate();
   const navigateToCheckout = () => {
     navigate("/checkout");
-  };
-
-  // To check if the added items in the cart have stock in place
-  // Params item, quantity and stock are interchangable in our codes
-  const addedToCart = (item, quantity) => {
-    if (item.stock < quantity) {
-      alert(`Only ${item.stock} items available in stock.`);
-      return;
-    }
-    setCartItems((prevItems) => {
-      [...prevItems, { ...item, quantity }];
-    });
-  };
-
-  // To edit the quantity of the added items
-  const updateQuantity = (index, quantity) => {
-    setCartItems((prevItems) => {
-      prevItems.map((item, i) => {
-        if (i === index) {
-          return { ...item, quantity };
-        }
-        return item;
-      });
-    });
   };
 
   // Total price of all the added items
@@ -60,47 +21,8 @@ const Cart = ({ shoppingCart }) => {
 
   let cartSum = sumCartTotal(shoppingCart);
 
-  // The first useEffect hook retrieves the saved cartItems from localStorage when the component is mounted.
-  // If there are saved items, it will set the state cartItems with the saved data.
-  useEffect(() => {
-    const savedCartItems = localStorage.getItem("cartItems");
-    if (savedCartItems) {
-      setCartItems(JSON.parse(savedCartItems));
-    }
-  }, []);
-
-  // The second useEffect hook updates the localStorage with the updated cartItems state whenever it changes.
-  useEffect(() => {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartItems]);
-
-  // checkout button click > route to checkout page
-  const handleClick = (event) => {
-    event.preventDefault();
-  };
-
   return (
     <div className="motion-safe:animate-fadeIn">
-      {/* // Updating the quantity via button function
-      {cartItems.map((item, index) => {
-        <div key={item.id}>
-          <p>{item.name}</p>
-          <button onClick={() => updateQuantity(index, item.quantity - 1)}>
-            -
-          </button>
-          <span>{item.quantity}</span>
-          <button onClick={() => updateQuantity(index, item.quantity + 1)}>
-            +
-          </button>
-        </div>;
-      })} */}
-
-      {/* // Showing the total price
-      <p>Total Price: {totalPrice}</p> */}
-
-      {/* // Button to checkout
-      <button onClick={handleCheckOut} /> */}
-
       {/* Start of CSS for View Cart with Items */}
       <div className="relative h-[15rem] w-full bg-cover bg-center">
         <div className="absolute text-lg font-permanentMarker text-darkBlue  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
--- src/Components/cart/CartSelfPickUp.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CartSelfPickUp = () => {
-  return <div>CartSelfPickUp</div>;
-};
-
-export default CartSelfPickUp;
