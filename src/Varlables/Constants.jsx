2ee433dc8023093fe1751cfebfbc84ede25e12ff
samuelphua <samuel.phua@hotmail.com>
2023-02-15 15:14:17 +0800

    Update Constants.jsx

--- src/Varlables/Constants.jsx
+++ src/Varlables/Constants.jsx
@@ -1,8 +1,8 @@
 import image1 from "../assets/imagesShop/image1.png";
 import image2 from "../assets/imagesShop/image2.png";
 import image3 from "../assets/imagesShop/image3.png";
-import image4 from "../assets/imagesShop/image4.png";
-import image5 from "../assets/imagesShop/image5.png";
+// import image4 from "../assets/imagesShop/image4.png";
+// import image5 from "../assets/imagesShop/image5.png";
 import image6 from "../assets/imagesShop/image6.png";
 import image7 from "../assets/imagesShop/image7.png";
 import image8 from "../assets/imagesShop/image8.png";
@@ -10,6 +10,7 @@ import image9 from "../assets/imagesShop/image9.png";
 import image10 from "../assets/imagesShop/image10.png";
 import image11 from "../assets/imagesShop/image11.png";
 import image12 from "../assets/imagesShop/image12.png";
+import image13 from "../assets/imagesShop/image13.png";
 
 // Product images for individual cookies
 import chocChipJar from "../assets/imagesProducts/ChocChip/Product-ChocChip-Jar.png";
@@ -32,8 +33,8 @@ export const images = [
   image1,
   image2,
   image3,
-  image4,
-  image5,
+  // image4,
+  // image5,
   image6,
   image7,
   image8,
@@ -41,14 +42,13 @@ export const images = [
   image10,
   image11,
   image12,
+  image13,
 ];
 
 export const description = [
   "PINEAPPLE SIGNATURE BALL",
   "CHOCOLATE CHIP COOKIES",
   "DOUBLE CHOCO CHIP COOKIES",
-  "MACADAMIA BUTTER COOKIES",
-  "ALMOND CHOCOLATE COOKIES",
   "MACADAMIA CHOCOLATE COOKIES",
   "SEA SALT CHOCOLATE COOKIES",
   "OATMEAL CRANBERRY COOKIES",
@@ -56,6 +56,7 @@ export const description = [
   "WHITE ALMOND COOKIE",
   "FRENCH SABLE COOKIES",
   "GINGERBREAD MAN",
+  "XMAS FRUIT CAKE",
 ];
 
 export const productImages = {
